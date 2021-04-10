9ea3c67dc21e775e14ff73d28e21f4325edebb4e
Bidida <hanafihamza1312@gmail.com>
2021-04-10 17:28:37 +0200

    renderVenue Function added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+
 // Foursquare API Info
+console.log("haza")
 const clientId = 'E1HXNVIENZHEIHXXLLSDL3DHLHCSPSDS5ZNAPNNZJYSSMN3X';
 const clientSecret = 'MU1XFB24LILVWHUHB203HS0SBPQBOZOBVLOITQOYRS12S353';
 const url = 'https://api.foursquare.com/v2/venues/explore?near=';
@@ -18,13 +20,35 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 
 // Add AJAX functions here:
  const getVenues = async () => {
-
-}
-
-const getForecast = () => {
   const city = $input.val();
   
   const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20210704`
+   try {
+     const respose = await fetch(urlToFetch)
+     if (respose.ok) {
+      const jsonResponse = await respose.json()
+      let venues = (jsonResponse.response.groups[0].items).map(e => e.venue)
+      console.log(venues)
+      return venues ; 
+    }
+   } catch (error) {
+     console.log(error)
+   }
+ }
+
+const getForecast = async () => {
+  const city = $input.val()
+  const  urlToFetch =   `${weatherUrl}?&q=${city}&APPID=${openWeatherKey}`
+  try {
+    const response = await fetch(urlToFetch)
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      console.log(jsonResponse)
+      return jsonResponse
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 
@@ -32,8 +56,11 @@ const getForecast = () => {
 const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
     // Add your code here:
+    const venue = venues[index]
+    const venueIcon = venue.categories[0].icon
+    const venueImgSrc = `${venueIcon.prefix}bg_64${venueIcon.suffix}`
 
-    let venueContent = '';
+    let venueContent = createVenueHTML(venue.name,venue.location,venueImgSrc);
     $venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
@@ -51,7 +78,7 @@ const executeSearch = () => {
   $weatherDiv.empty();
   $destination.empty();
   $container.css("visibility", "visible");
-  getVenues()
+  getVenues().then(response => renderVenues(response))
   getForecast()
   return false;
 }
